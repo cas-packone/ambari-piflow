85b11884e2db8c404373b588eaa571882c4efb85
or <43975495+yg000@users.noreply.github.com>
2019-06-27 14:54:15 +0800

    Update piflow_server.py

--- package/scripts/piflow_server.py
+++ package/scripts/piflow_server.py
@@ -13,7 +13,7 @@ class PiFlowServerMaster(Script):
         )
         Execute('cd /data/piflow/')
         Execute('cd /data/piflow/ && wget https://github.com/cas-bigdatalab/piflow/releases/download/v0.5/piflow.tar.gz')
-        Execute('cd /data/piflow/ && tar -xvf piflow.tar.gz && mv piflow piflowSever')
+        Execute('cd /data/piflow/ && tar -xvf piflow.tar.gz && mv piflow piflowServer')
         Execute('cd /data/piflow/ && rm -rf  piflow.tar.gz')
         Execute('sudo -u hdfs hdfs dfs -mkdir -p /apps/piflow/checkpoints/')
         Execute('sudo -u hdfs hdfs dfs -mkdir -p /apps/piflow/debug/')
@@ -23,20 +23,20 @@ class PiFlowServerMaster(Script):
         import params
         env.set_params(params)
         piflow_sever = InlineTemplate(params.piflow_sever)   
-        File(format("/data/piflow/piflowSever/config.properties"), content=piflow_sever)
+        File(format("/data/piflow/piflowServer/config.properties"), content=piflow_sever)
 
     def start(self, env):
         import params
         env.set_params(params)
         self.configure(env)
-        Execute('cd /data/piflow/piflowSever/ && nohup sh start.sh > nohup.out 2>&1 &', ignore_failures=True)
-        Execute("ps -ef | grep java | grep piflow-server-0.9.jar | grep -v grep | awk '{print $2}' >/data/piflow/piflowSever/piflowSever.pid")
+        Execute('cd /data/piflow/piflowServer/ && nohup sh start.sh > nohup.out 2>&1 &', ignore_failures=True)
+        Execute("ps -ef | grep java | grep piflow-server-0.9.jar | grep -v grep | awk '{print $2}' >/data/piflow/piflowServer/ps.pid")
       
     def stop(self, env):
         import params
         env.set_params(params)
         self.configure(env)
-        Execute('cd /data/piflow/piflowSever/ && sh stop.sh', ignore_failures=True)
+        Execute('cd /data/piflow/piflowServer/ && sh stop.sh', ignore_failures=True)
 
 
     def restart(self, env):
@@ -44,7 +44,7 @@ class PiFlowServerMaster(Script):
         self.start(env)
 
     def status(self, env):
-        check_process_status("/data/piflow/piflowSever/piflowSever.pid")
+        check_process_status("/data/piflow/piflowServer/ps.pid")
 
 
 if __name__ == "__main__":
