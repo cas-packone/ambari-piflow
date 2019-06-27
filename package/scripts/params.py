d612f2d595b82307ac038e5bf5fb9c386f91d055
or <43975495+yg000@users.noreply.github.com>
2019-06-27 15:13:15 +0800

    Update params.py

--- package/scripts/params.py
+++ package/scripts/params.py
@@ -5,6 +5,6 @@ import sys, os, glob,socket
 # server configurations
 config = Script.get_config()
 service_packagedir = os.path.realpath(__file__).split('/scripts')[0]
-piflow_sever=config['configurations']['piflow']['piflow_sever']
+piflow_server=config['configurations']['piflow']['piflow_server']
 piflow_web=config['configurations']['piflow']['piflow_web']
 current_host_name = socket.gethostname()
